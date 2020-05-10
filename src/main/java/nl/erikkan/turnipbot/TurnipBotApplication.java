507deff3ccfbac0b33db07540e2bc030d1181846
es-kan <info@erikkan.nl>
2020-05-10 11:08:36 +0200

    initial commit

---
+++ src/main/java/nl/erikkan/turnipbot/TurnipBotApplication.java
@@ -0,0 +1,13 @@
+package nl.erikkan.turnipbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnipBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnipBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/erikkan/turnipbot/TurnipBotApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.erikkan.turnipbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnipBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
